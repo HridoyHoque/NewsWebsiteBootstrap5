42a9db4338c59fc050b63a66a9345c4889b661fe
HridoyHoque <hhridoy155@gmail.com>
2023-03-02 16:10:26 -0700

    Added Categories section

--- news.js
+++ news.js
@@ -1,1 +1,19 @@
-console.log("connected")
+const loadCatagories = () => {
+
+    fetch("https://openapi.programming-hero.com/api/news/categories")
+        .then(res => res.json())
+        .then(data => showcategories(data.data))
+}
+
+const showcategories = data => {
+    // console.log(data)
+    const cetagoriesContainer = document.getElementById("catagories-container")
+    data.news_category.forEach(singleCetagory => {
+        // console.log(singleCetagory)
+        cetagoriesContainer.innerHTML += `
+        <a class="nav-link" href="#">${singleCetagory.category_name}</a>
+        `
+    });
+}
+
+// loadCatagories()
