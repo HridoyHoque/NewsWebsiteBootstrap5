8c3ad250200f6d4b298ccb7295d92a7f6dc3f118
HridoyHoque <hhridoy155@gmail.com>
2023-03-02 17:13:27 -0700

    Added categories Alert Section

--- news.js
+++ news.js
@@ -6,14 +6,27 @@ const loadCatagories = () => {
 }
 
 const showcategories = data => {
-    // console.log(data)
+    console.log(data)
     const cetagoriesContainer = document.getElementById("catagories-container")
     data.news_category.forEach(singleCetagory => {
         // console.log(singleCetagory)
         cetagoriesContainer.innerHTML += `
-        <a class="nav-link" href="#">${singleCetagory.category_name}</a>
+        <a class="nav-link" href="#" onclick="fetchCategoriesNews('${singleCetagory.category_id}')">${singleCetagory.category_name}</a>
         `
     });
 }
 
-// loadCatagories()
+// fetch All Newses Avialable in categories
+ const fetchCategoriesNews = category_id => {
+//   console.log(category_id)
+fetch(`https://openapi.programming-hero.com/api/news/category/${category_id}`)
+.then(res => res.json())
+.then(data => showAllNews(data , category_id))
+
+ }
+
+ const showAllNews = (data , category_id) => {
+    console.log(data , category_id)
+    document.getElementById("category-count").innerText= data.data.length
+    document.getElementById("category-name").innerText= category_id
+ }
